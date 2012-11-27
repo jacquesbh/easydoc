e89b7d699a716dcabfbeb4b49863c8cabc656793
Jacques Bodin-Hullin <jacques@bodin-hullin.net>
2012-11-28 00:38:12 +0100

    Easydoc\Exception added.

---
+++ lib/Easydoc/Exception.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Easydoc.
+ *
+ * @license Lesser General Public License v3 (http://www.gnu.org/licenses/lgpl-3.0.txt)
+ * @author Jacques Bodin-Hullin <jacques@bodin-hullin.net>
+ * @copyright Copyright (c) 2012 Jacques Bodin-Hullin (http://jacques.sh/)
+ */
+
+namespace Easydoc;
+
+/**
+ * The Easydoc exception class \o/
+ *
+ * @package Easydoc
+ */
+class Exception extends \Exception
+{}
